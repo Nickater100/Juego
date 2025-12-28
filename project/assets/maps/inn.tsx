561e62486c28bb996ed533c686da9093f55e743f
nickater1100 <nicosuasnavar@gmail.com>
2025-12-28 14:27:07 -0300

    nuevo mapa

---
+++ project/assets/maps/inn.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="inn" tilewidth="32" tileheight="32" tilecount="308" columns="22">
+ <image source="../tilesets/inn.png" width="707" height="468"/>
+</tileset>
