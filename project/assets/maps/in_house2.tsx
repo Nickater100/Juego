4633393dd31f52f7d680657a49695825b544c92e
nickater1100 <nicosuasnavar@gmail.com>
2025-12-27 23:14:00 -0300

    pueblo y army

---
+++ project/assets/maps/in_house2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="in_house2" tilewidth="32" tileheight="32" tilecount="532" columns="28">
+ <image source="../tilesets/in_house2.png" width="913" height="608"/>
+</tileset>
