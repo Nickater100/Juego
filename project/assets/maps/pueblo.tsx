d277bd567d4de9ef5624257f5376e3abe8934193
nickater1100 <nicosuasnavar@gmail.com>
2025-12-27 22:25:43 -0300

    pueblo mapa

---
+++ project/assets/maps/pueblo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="pueblo" tilewidth="32" tileheight="32" tilecount="1536" columns="48">
+ <image source="../tilesets/pueblo.png" width="1536" height="1024"/>
+</tileset>
